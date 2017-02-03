c178c29a4c0b250d8de9d8b7843c85b8e0b5aba4
nsearch101 <niche.search101@gmail.com>
2017-02-03 14:45:09 +0000

    style login page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def sign_up_text
+    raw('<span class="glyphicon glyphicon-user"></span> Sign Up')
+  end
+  
+  def login_text
+    raw('<span class="glyphicon glyphicon-login"></span> Login')
+  end
 end
