c7963f6c05998e65b85571e4dc6b2a0a649d201b
nsearch101 <niche.search101@gmail.com>
2017-02-03 21:01:41 +0000

    add notes action and views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,16 +1,24 @@
 class NotesController < ApplicationController
+  before_action :find_note, only: [:show, :edit, :update, :destroy]
 
   def index
-    @notes = Note.order('created_at DESC')
+    @notes = current_user.notes.order('created_at DESC')
   end
   
   def show
   end
   
   def new
+    @note = Note.new
   end
   
   def create
+    @note = current_user.notes.build(note_params)
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
   end
   
   def edit
@@ -25,8 +33,10 @@ class NotesController < ApplicationController
   private
   
   def find_note
+    @note = Note.find(params[:id])
   end
   
   def note_params
+    params.require(:note).permit(:title, :content)
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,6 +7,10 @@ module ApplicationHelper
     raw('<span class="glyphicon glyphicon-log-in"></span> Login')
   end
   
+  def new_note_text
+    raw('<span class="glyphicon glyphicon-list-alt"></span> New Note')
+  end
+  
   def logout_text
     raw('<span class="glyphicon glyphicon-log-out"></span> Logout')
   end
