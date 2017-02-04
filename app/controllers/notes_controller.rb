5670cc72f5bef134c7eea80e511c7a3101820dfc
michael-palad <happyfighter2015@gmail.com>
2017-02-04 13:08:42 +0000

    add category field in forms

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -44,6 +44,6 @@ class NotesController < ApplicationController
   end
   
   def note_params
-    params.require(:note).permit(:title, :content)
+    params.require(:note).permit(:title, :content, :category_id)
   end
 end
