b023d850a5cad4e0c89789f965e49696ce2f5dbc
michael-palad <happyfighter2015@gmail.com>
2017-02-04 14:22:11 +0000

    add create category

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -13,22 +13,48 @@ class NotesController < ApplicationController
   end
   
   def create
-    @note = current_user.notes.build(note_params)
-    if @note.save
-      redirect_to @note
+    if add_category?
+      @note = Note.new(note_params)
+      @category = Category.new(name: note_params[:new_category], user: current_user)
+      if @category.save
+        @note.category = @category
+        render 'new'
+      else
+        flash[:alert] = "Unable to Create Category"
+        render 'new'
+      end
     else
-      render 'new'
+      @note = current_user.notes.build(note_params)
+      if @note.save
+        redirect_to @note
+      else
+        render 'new'
+      end
     end
+    
   end
   
   def edit
   end
   
   def update
-    if @note.update(note_params)
-      redirect_to @note
+    if add_category?
+      @note = Note.new(note_params)
+      @category = Category.new(name: note_params[:new_category], user: current_user)
+      if @category.save
+        @note.category = @category
+        render 'edit'
+      else
+        flash[:alert] = "Unable to Create Category"
+        render 'edit'
+      end
+      
     else
-      render 'edit'
+      if @note.update(note_params)
+        redirect_to @note
+      else
+        render 'edit'
+      end
     end
   end
   
@@ -44,6 +70,10 @@ class NotesController < ApplicationController
   end
   
   def note_params
-    params.require(:note).permit(:title, :content, :category_id)
+    params.require(:note).permit(:title, :content, :category_id, :new_category)
+  end
+  
+  def add_category?
+    params[:commit] == "Add Category"
   end
 end
--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,6 @@
 class Note < ApplicationRecord
+  attr_accessor :new_category  # for creating categories
+  
   belongs_to :user
   belongs_to :category
 end
